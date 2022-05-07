a79017c6adb1bea77fb545342225d6f801b52f1f
jbkwizera <jeanbaptistekwi@gmail.com>
2022-05-07 23:50:21 +0200

    Add services to use db

---
+++ src/services/workout-service.js
@@ -0,0 +1,29 @@
+const workout = require("../database/workout");
+
+const getAllWorkouts = () => {
+    return workout.getAllWorkouts();
+};
+
+const getOneWorkout = () => {
+    return;
+};
+
+const createNewWorkout = () => {
+    return;
+};
+
+const updateOneWorkout = () => {
+    return;
+};
+
+const deleteOneWorkout = () => {
+    return;
+};
+
+module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
+};
